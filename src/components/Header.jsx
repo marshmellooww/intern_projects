e59d1f358beb1e83ef0dd80edf651d83bef6b2cd
marshmellooww <80846061+marshmellooww@users.noreply.github.com>
2023-02-28 01:56:17 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon></EventNoteIcon>
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
